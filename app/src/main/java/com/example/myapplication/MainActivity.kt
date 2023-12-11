e4854c0af8e17f535059981a85c76b5269626f3e
JoseSalinas <2023_jose.salinas@iticbcn.cat>
2023-12-11 04:00:03 +0100

    Uso de estilos, drawables, layouts avanzados

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,109 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.SurfaceControl.Transaction
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var binding:ActivityMainBinding
+
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //Activar Fragmentos de navbar inferior
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        drawerLayout = findViewById(R.id.lateral_menu_drawer)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val navigationView = findViewById<NavigationView>(R.id.lateral_nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+        val toogle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+        if (savedInstanceState == null) {
+            replaceFragment(ShopFragment())
+        }
+
+
+
+        //Se reecge vista de navbar inferior Para visualizar iconos color original
+        val bnv: BottomNavigationView = binding.bottomNavigationView
+        bnv.itemIconTintList = null
+
+        //Para colorear titulos
+        paintTitle(binding.shopTitle)
+
+
+        replaceFragment(HomeFragment())
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home_button -> replaceFragment(HomeFragment())
+                R.id.selected_button -> replaceFragment(SelectedFragment())
+                R.id.personal_button -> replaceFragment(PersonalFragment())
+                else -> {
+
+                }
+            }
+            true
+        }
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.shop_zone -> replaceFragment(ShopFragment())
+            R.id.chat_zone -> replaceFragment(ChatFragment())
+            R.id.wiki_zone -> replaceFragment(WikiFragment())
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+    private fun replaceLateralFragment(fragment: Fragment) {
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.lateral_menu_fragment_container, fragment)
+        transaction.commit()
+    }
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_container, fragment)
+        fragmentTransaction.commit()
+
+    }
+
+    fun paintTitle(textview: TextView) {
+        val myShader: Shader = LinearGradient(
+            0f, 0f, 0f, 100f,
+            Color.rgb(255, 250, 160), Color.rgb(10, 0, 46),
+            Shader.TileMode.CLAMP
+        )
+        textview.paint.setShader(myShader)
     }
 }
